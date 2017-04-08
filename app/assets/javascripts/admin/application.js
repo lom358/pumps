7c55355c347a9ca4a8ab8e255629b8970f783d43
lom358 <Lukashin357@yandex.ru>
2017-04-08 21:13:32 +0300

    add page export import docs

--- app/assets/javascripts/admin/application.js
+++ app/assets/javascripts/admin/application.js
@@ -2,10 +2,35 @@
 //= require jquery_ujs
 //= require_tree .
 
-
+//Без особой заморочки постраничного разграничения срабатывания скрипта
 ADMIN = {
 
     init:function () {
+        ADMIN.init_btn();
+    },
+
+    init_btn : function () {
+        $('#download_csv').click(function () {
+            alert('download_csv');
+        });
+
+        $('#download_exl').click(function () {
+            alert('download_exl ');
+        });
+    },
+
+    //AJAX 
+    post: function(data, success, error) {
+        $.ajax({
+            url: '/send_form',
+            type: 'POST',
+            data: data,
+            timeout: 30000,
+            dataType: 'json',
+            success: success,
+            error: error
+        })
+
 
     }
 
